4da7a7ae9be255cbfa54ed4e14864b0cddc422c1
Abhiyodaya2002 <abhiyodayapandey@gmail.com>
2023-07-21 16:49:12 +0530

    first

---
+++ client/src/Components/TypingHome.jsx
@@ -0,0 +1,22 @@
+import "../StyleSheets/TypingHome.css";
+
+import React from "react";
+
+const TypingHome = () => {
+  return (
+    <>
+      <div className="home-navbar-container">
+        <div className="home-navbar-left-container">
+          <a href="/" style={{fontSize: "1.8rem"}}>Typing Dash Pro</a>
+        </div>
+        <div className="home-navbar-right-container">
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TypingHome;
